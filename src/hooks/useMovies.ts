3008fe14e01f05effa78ab15dd24ebd2b1063371
root <root@ProCoding>
2025-06-01 00:57:01 +0200

    feat: sort movies

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_BASE_URL } from "@/config/constants";
 
-export function useMovies(token?: string) {
+export function useMovies(sortBy: string, sortDir: string, token?: string) {
+  console.log(`${API_BASE_URL}/movies/sort_by=${sortBy}&sort_dir=${sortDir}`);
   return useQuery({
-    queryKey: ["userMovies", token],
+    queryKey: ["userMovies", token, sortBy, sortDir],
     queryFn: async () => {
-      const res = await fetch(`${API_BASE_URL}/movies`, {
-        headers: token ? { Authorization: `Bearer ${token}` } : {},
-      });
+      const res = await fetch(
+        `${API_BASE_URL}/movies/?sort_by=${sortBy}&sort_dir=${sortDir}`,
+        {
+          headers: token ? { Authorization: `Bearer ${token}` } : {},
+        },
+      );
       if (!res.ok) throw new Error("Not authenticated");
       return res.json();
     },
