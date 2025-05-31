1c14eac8677fa38cfbca4aea5a8d1eb9e3dea626
root <root@ProCoding>
2025-05-31 23:17:09 +0200

    refactor: change movie card colors to match dark theme

--- src/app/browse_movies/page.tsx
+++ src/app/browse_movies/page.tsx
@@ -28,20 +28,22 @@ export default function BrowseMovies() {
 
   const { data: movies_data, isLoading } = useMovies(token);
 
-  if (!user) return null;
+  if (!user) {
+    return loading();
+  }
 
   if (isLoading || !movies_data) return loading();
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mx-24 mb-16">
       {(movies_data as Movie[]).map((movie) => (
-        <Card key={movie.id} className="p-4">
-          <CardHeader className="flex items-center justify-between">
+        <Card key={movie.id} className="p-4 bg-zinc-800 text-white">
+          <CardHeader className="flex items-center justify-between mt-2">
             <CardTitle>
               <h1 className="text-2xl font-semibold">{movie.title}</h1>
             </CardTitle>
             <button>
-              <Pencil className="w-6 h-6 text-gray-500 hover:text-gray-700" />
+              <Pencil className="w-6 h-6 text-white hover:text-gray-200" />
             </button>
           </CardHeader>
 
@@ -52,25 +54,25 @@ export default function BrowseMovies() {
               className="h-[200px] w-auto rounded object-contain mx-auto mb-4"
             />
 
-            <div className="flex gap-4 w-full text-center mt-6">
+            <div className="flex gap-4 w-full text-center mt-8">
               <ItemWithIcon
-                icon={<Clock4 className="w-6 h-6 text-gray-600" />}
+                icon={<Clock4 className="w-6 h-6 text-white" />}
                 label={`${movie.duration} seconds`}
               />
               <ItemWithIcon
-                icon={<Calendar className="w-6 h-6 text-gray-600" />}
+                icon={<Calendar className="w-6 h-6 text-white" />}
                 label={movie.created_at}
               />
               <ItemWithIcon
-                icon={<Languages className="w-6 h-6 text-gray-600" />}
+                icon={<Languages className="w-6 h-6 text-white" />}
                 label={movie.native_lang}
               />
             </div>
 
             {movie.description && movie.description.trim() !== "" && (
-              <CardDescription className="mt-4">
-                <h1 className="text-lg font-bold text-black">Description</h1>
-                <p className="text-base text-justify text-gray-600">
+              <CardDescription className="mt-6">
+                <h1 className="text-lg font-bold text-white">Description</h1>
+                <p className="text-base text-justify text-white mt-1">
                   {movie.description}
                 </p>
               </CardDescription>
